0783fcad3043674a77b7f5e98a8f3024cab91981
tomdamo <20182376@student.hhs.nl>
2022-06-28 20:25:43 +0200

    Gebruiker kan iemand laten abonneren

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,65 @@ import java.util.Scanner;
 public class Main {
     static ProductVoorraad<Personenauto> pv = PAutoVoorraad.getInstance();
     static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         seed();
         start();
 
     }
 
-    public static void start(){
+    public static void start() throws InterruptedException {
         System.out.println("Welkom bij het overzichtscherm, de volgende producten zijn beschikbaar:");
         printAuto();
         System.out.println("");
-        System.out.println("Wat wilt u doen? \n Type X om een auto toe te voegen. \n Type Y om een gebruiker te laten abboneren");
-        String input = scanner.nextLine();
+        System.out.println("Wat wilt u doen? \n Type 1 om een auto toe te voegen. \n Type 2 om een auto te verhuren \n Type 3 om een gebruiker te laten abonneren");
 
-        if (input.equals("x") || input.equals("X")){
-            voegToe();
+        int input = scanner.nextInt();
+        scanner.nextLine();
+        switch(input){
+            case 1:
+                voegToe();
+                break;
+            case 2:
+                verhuren();
+                break;
+            case 3:
+                abboneren();
+                break;
+            default:
+                start();
         }
-        if (input.equals("y") || input.equals("Y")){
 
+
+
+
+    }
+
+    private static void verhuren() {
+    }
+
+    private static void abboneren() throws InterruptedException {
+        try {
+
+            System.out.println("");
+            System.out.println("Wie wilt abonneren?");
+            String naam = scanner.nextLine();
+            Subscriber s1 = new Subscriber(naam);
+            System.out.println("Van welke auto wilt " + naam + " van op de hoogte zijn?(Voer getal vd rij in)");
+            printAuto();
+            int gekozen = scanner.nextInt();
+            scanner.nextLine();
+            s1.subscribeProduct(pv.getProduct(gekozen));
+            pv.getProduct(gekozen).subscribe(s1);
+            System.out.println("Succesvol geabonneerd");
+            pv.getProduct(gekozen).notifySubscribers();
+            Thread.sleep(3000);
+            System.out.println("");
+            start();
         }
-        else{
-            System.out.println("Programma stopt");
-            System.exit(0);
+        catch (Exception e){
+            System.out.println("oops!");
         }
-
-
     }
 
     private static void voegToe() {
@@ -38,7 +71,9 @@ public class Main {
             String merk = scanner.nextLine();
             System.out.println("Voer de gewicht van de auto in: (hele getal)");
             int gewicht = scanner.nextInt();
+
             pv.toevoegen(new Personenauto(merk,gewicht));
+            scanner.nextLine();
             start();
         }
         catch(Exception e) {
